c8543678e19598b5948d0036e3f6d30272ae716b
John Ferdie Abueg <john.abueg@gmail.com>
2024-10-18 10:02:05 +0200

    skeleton post repo

---
+++ cohort-backend.wwwapi/Repository/IPostRepository.cs
@@ -0,0 +1,20 @@
+﻿using cohort_backend.wwwapi.Models;
+
+namespace cohort_backend.wwwapi.Repository
+{
+    public interface IPostRepository
+    {
+        // POSTS
+        Task<Post> CreatePost(Post entity);
+        Task<IEnumerable<Post>> GetAllPosts();
+        Task<Post> UpdatePostById(Post entity, int postId);
+        Task<Post> DeletePostById(int postId);
+        Task<Post> GetPostById(int postId);
+
+        // COMMENTS
+        Task<Comment> CreateCommentInPost(Comment entity, int postId);
+        Task<IEnumerable<Comment>> GetAllCommentsInPost(int postId);
+        Task<Comment> UpdateCommentInPost(Comment entity, int postId);
+        Task<Comment> DeleteCommentInPost(int commentId, int postId);
+    }
+}
---
+++ cohort-backend.wwwapi/Repository/PostRepository.cs
@@ -0,0 +1,62 @@
+﻿using cohort_backend.wwwapi.Data;
+using cohort_backend.wwwapi.Models;
+
+namespace cohort_backend.wwwapi.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        private DatabaseContext _db;
+
+        public PostRepository(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        // POSTS
+        public async Task<Post> CreatePost(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Post>> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post> UpdatePostById(Post entity, int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post> DeletePostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post> GetPostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // COMMENTS
+        public async Task<Comment> CreateCommentInPost(Comment entity, int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllCommentsInPost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Comment> UpdateCommentInPost(Comment entity, int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Comment> DeleteCommentInPost(int commentId, int postId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
