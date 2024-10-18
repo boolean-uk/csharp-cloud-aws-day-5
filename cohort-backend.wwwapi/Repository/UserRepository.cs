df26e93375fa5c4327f35845702cedb6d6808f51
bjorgastrid <bjorgkrist@hotmail.com>
2024-10-18 10:41:03 +0200

    enpoint set up

--- cohort-backend.wwwapi/Repository/UserRepository.cs
+++ cohort-backend.wwwapi/Repository/UserRepository.cs
@@ -7,15 +7,10 @@ namespace cohort_backend.wwwapi.Repository
     public class UserRepository : IUserRepository
     {
         private DatabaseContext _databaseContext;
-        
-        public UserRepository (DatabaseContext db)
+
+        public UserRepository(DatabaseContext db)
         {
             _databaseContext = db;
-        }
-
-        public async Task<User> GetPrimeUser(int id)
-        {
-            return await _databaseContext.Users.FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<User> GetUserById(int id)
